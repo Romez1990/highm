2ac8fafafc62daedae4bb960e48c5c563923e394
Romez <Romez1990@gmail.com>
2020-01-18 20:03:22 +0300

    feature(App): install material ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { AppContext } from 'next/app';
 import { NextComponentType } from 'next/dist/next-server/lib/utils';
+import ThemeProvider from '../components/App/ThemeProvider';
 
 interface InitialProps {
   pageProps: object;
@@ -25,7 +26,19 @@ App.getInitialProps = async ({
 };
 
 function App({ Component: Page, pageProps }: Props): JSX.Element {
-  return <Page {...pageProps} />;
+  useEffect(removeStyles, []);
+
+  function removeStyles(): void {
+    const jssStyles = document.querySelector('#jss-server-side');
+    // eslint-disable-next-line no-unused-expressions
+    jssStyles?.remove();
+  }
+
+  return (
+    <ThemeProvider>
+      <Page {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { Children, ReactElement } from 'react';
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
 import {
   DocumentContext,
   DocumentInitialProps,
+  RenderPageResult,
 } from 'next/dist/next-server/lib/utils';
+import { ServerStyleSheets } from '@material-ui/core/styles';
+import { StylesProviderProps } from '@material-ui/styles/StylesProvider';
+import theme from '../src/Theme';
 
 class Document extends NextDocument {
   public static async getInitialProps(
     ctx: DocumentContext,
   ): Promise<DocumentInitialProps> {
+    const originalRenderPage = ctx.renderPage;
+    const sheets = new ServerStyleSheets();
+    ctx.renderPage = (): RenderPageResult | Promise<RenderPageResult> =>
+      originalRenderPage({
+        enhanceApp: App => (props): ReactElement<StylesProviderProps> =>
+          sheets.collect(<App {...props} />),
+      });
+
     const initialProps = await super.getInitialProps(ctx);
 
     return {
       ...initialProps,
+      styles: [
+        ...Children.toArray(initialProps.styles),
+        sheets.getStyleElement(),
+      ],
     };
   }
 
@@ -20,9 +36,15 @@ class Document extends NextDocument {
     return (
       <Html>
         <Head>
+          {/* PWA primary color */}
+          <meta name="theme-color" content={theme.palette.primary.main} />
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1.0, minimum-scale=1.0, shrink-to-fit=no"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
         </Head>
         <body>
