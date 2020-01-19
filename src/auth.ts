290b67984a15d7e1dc68b0fdc79369abc38e22bc
Romez <Romez1990@gmail.com>
2020-01-19 11:42:47 +0300

    feature(project): create authenticate function

--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,27 @@
 import { IncomingMessage } from 'http';
+import { createAxios } from './axios';
 import { getCookie } from './cookies';
+import Profile from '../types/Profile';
 
 function getToken(req?: IncomingMessage): string | undefined {
   return getCookie('token', req);
 }
 
+async function authenticate(req?: IncomingMessage): Promise<Profile | undefined> {
+  const token = getToken(req);
+  if (!token) return;
+
+  const axios = createAxios(req);
+  let response;
+  try {
+    response = await axios.get('/auth/profile/');
+  } catch (err) {
+    return;
+  }
+  return response.data;
+}
+
 export {
   getToken,
+  authenticate,
 };
