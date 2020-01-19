2c4349e392b90a1520336a919f7e89edb438bd43
Romez <Romez1990@gmail.com>
2020-01-19 11:20:07 +0300

    feature(project): create create axios function with token

--- src/axios.ts
+++ src/axios.ts
@@ -1,5 +1,7 @@
+import { IncomingMessage } from 'http';
 import axiosStatic, { AxiosRequestConfig } from 'axios';
 import applyConverters from 'axios-case-converter';
+import { getToken } from './auth';
 
 const baseConfig: AxiosRequestConfig = {
   baseURL: `${process.env.SERVER_URL}/api`,
@@ -7,4 +9,22 @@ const baseConfig: AxiosRequestConfig = {
 
 const axios = applyConverters(axiosStatic.create(baseConfig) as any);
 
+function createAxios(req?: IncomingMessage) {
+  let config = baseConfig;
+  const token = getToken(req);
+  if (token) {
+    const configWithAuth = {
+      headers: {
+        Authorization: `Token ${token}`,
+      },
+    };
+    config = { ...baseConfig, ...configWithAuth };
+  }
+  return applyConverters(axiosStatic.create(config) as any);
+}
+
 export default axios;
+
+export {
+  createAxios,
+};
