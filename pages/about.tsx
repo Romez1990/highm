0f49b0e541a772d89cecc7af216b89ad00042115
Romez <Romez1990@gmail.com>
2020-01-21 07:23:07 +0300

    feature(aboutPage): create about page

---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainLayout from '../layouts/MainLayout';
+
+function AboutPage() {
+  return (
+    <MainLayout title="About page">
+      About page
+    </MainLayout>
+  );
+}
+
+export default AboutPage;
