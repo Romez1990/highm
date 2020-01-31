47bb0e5280b31e2ba6a46f3f5de3f38c6a3293ae
Romez <Romez1990@gmail.com>
2020-01-31 21:33:12 +0300

    feature(project): create apply permission function

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,8 @@
-import { IncomingMessage } from 'http';
+import { IncomingMessage, ServerResponse } from 'http';
 import { createAxios } from './axios';
 import { getCookie } from './cookies';
-import Profile from '../types/Profile';
+import { redirectToLogin } from './redirect';
+import Profile, { User } from '../types/Profile';
 
 function getToken(req?: IncomingMessage): string | undefined {
   return getCookie('token', req);
@@ -21,7 +22,43 @@ async function authenticate(req?: IncomingMessage): Promise<Profile | undefined>
   return response.data;
 }
 
+type Permission =
+  | 'IsAuthenticated'
+  | 'IsStudent'
+  | 'IsTeacher'
+  | 'IsAdmin'
+  ;
+
+async function applyPermission(
+  profile: Profile | undefined,
+  permission: Permission,
+  req?: IncomingMessage,
+  res?: ServerResponse,
+): Promise<void> {
+  if (!hasPermission(profile, permission))
+    await redirect(req, res);
+}
+
+function hasPermission(user: Profile | User | undefined, permission: Permission): boolean {
+  if (!user) return false;
+  return user.type === 'admin' ||
+    permission === 'IsAuthenticated' ||
+    user.type === 'teacher' && permission === 'IsTeacher' ||
+    user.type === 'student' && permission === 'IsStudent';
+}
+
+async function redirect(
+  req?: IncomingMessage,
+  res?: ServerResponse,
+): Promise<void> {
+  if (process.browser || !req)
+    throw new Error('redirecting from the browser is unacceptable');
+  await redirectToLogin(res, req.url);
+}
+
 export {
   getToken,
   authenticate,
+  applyPermission,
+  hasPermission,
 };
