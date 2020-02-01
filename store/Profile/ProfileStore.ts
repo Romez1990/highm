c5b77b201e2a7977cfa96b7e8119db31be452728
Romez <Romez1990@gmail.com>
2020-02-01 14:18:07 +0300

    feature(ProfileStore): create authenticate action

--- store/Profile/ProfileStore.ts
+++ store/Profile/ProfileStore.ts
@@ -1,5 +1,9 @@
+import { IncomingMessage } from 'http';
 import { observable, action } from 'mobx';
+import { pipe } from 'fp-ts/lib/pipeable';
 import { Option, none } from 'fp-ts/lib/Option';
+import { Task, map } from 'fp-ts/lib/Task';
+import AuthenticationService from '../../src/AuthenticationService';
 import { Profile } from '../../src/Profile';
 
 class ProfileStore {
@@ -9,6 +13,13 @@ class ProfileStore {
   @action
   public setProfile(profile: Option<Profile>): void {
     this.profile = profile;
+  }
+
+  public authenticate(req: IncomingMessage | undefined): Task<void> {
+    return pipe(
+      AuthenticationService.authenticate(req),
+      map(profile => this.setProfile(profile)),
+    );
   }
 
   public hydrate(profileStore: ProfileStore): void {
