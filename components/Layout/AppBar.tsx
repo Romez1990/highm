d94e0912aaa46bf9cceb73f0b9374214c53882ba
Romez <Romez1990@gmail.com>
2020-02-20 17:26:33 +0300

    feature(Layout): create drawer

--- components/Layout/AppBar.tsx
+++ components/Layout/AppBar.tsx
@@ -2,26 +2,45 @@ import React from 'react';
 import {
   makeStyles,
   createStyles,
+  Theme,
   AppBar as MuiAppBar,
   Toolbar,
   Typography,
+  IconButton,
 } from '@material-ui/core';
+import { Menu as MenuIcon } from '@material-ui/icons';
 
-const useStyles = makeStyles(
+interface Props {
+  toggleDrawerOpen(): void;
+}
+
+const useStyles = makeStyles(({ spacing, zIndex }: Theme) =>
   createStyles({
+    appBar: {
+      zIndex: zIndex.drawer + 1,
+    },
     title: {
+      marginLeft: spacing(2),
       flexGrow: 1,
       fontSize: '1.5rem',
     },
   }),
 );
 
-function AppBar(): JSX.Element {
+function AppBar({ toggleDrawerOpen }: Props): JSX.Element {
   const classes = useStyles();
 
   return (
-    <MuiAppBar position="static">
+    <MuiAppBar position="fixed" className={classes.appBar}>
       <Toolbar>
+        <IconButton
+          edge="start"
+          color="inherit"
+          onClick={toggleDrawerOpen}
+          aria-label="menu"
+        >
+          <MenuIcon />
+        </IconButton>
         <Typography component="h1" variant="h6" className={classes.title}>
           HighM
         </Typography>
--- layouts/MainLayout.tsx
+++ layouts/MainLayout.tsx
@@ -1,12 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { makeStyles, createStyles, Theme } from '@material-ui/core';
+import clsx from 'clsx';
 import AppBar from '../components/Layout/AppBar';
+import Drawer from '../components/Layout/Drawer';
 import BaseLayout, { LayoutProps } from './BaseLayout';
 
+interface StyleProps {
+  drawerWidth: number;
+}
+
+const useStyles = makeStyles(
+  ({ spacing, transitions, mixins: { toolbar } }: Theme) =>
+    createStyles({
+      toolbar,
+      content: {
+        paddingTop: spacing(2),
+        transition: transitions.create('margin', {
+          easing: transitions.easing.sharp,
+          duration: transitions.duration.leavingScreen,
+        }),
+      },
+      contentShift: ({ drawerWidth }: StyleProps) => ({
+        marginLeft: drawerWidth,
+        transition: transitions.create('margin', {
+          easing: transitions.easing.easeOut,
+          duration: transitions.duration.enteringScreen,
+        }),
+      }),
+    }),
+);
+
 function MainLayout({ title, children }: LayoutProps): JSX.Element {
+  const [open, setOpen] = useState(false);
+
+  function toggleDrawerOpen(): void {
+    setOpen(!open);
+  }
+
+  const drawerWidth = 240;
+
+  const classes = useStyles({ drawerWidth });
+
   return (
     <BaseLayout title={title}>
-      <AppBar />
-      {children}
+      <AppBar toggleDrawerOpen={toggleDrawerOpen} />
+      <Drawer width={drawerWidth} open={open} />
+      <div className={classes.toolbar} />
+      <main
+        className={clsx(classes.content, {
+          [classes.contentShift]: open,
+        })}
+      >
+        {children}
+      </main>
     </BaseLayout>
   );
 }
