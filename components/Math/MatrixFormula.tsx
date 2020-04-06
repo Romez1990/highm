f3719477571272c929978e8ad1133efdccbe2c2d
Romez <Romez1990@gmail.com>
2020-04-06 10:51:33 +0300

    feature(MatrixFormula): allow specify bracket

--- components/Math/MatrixFormula.tsx
+++ components/Math/MatrixFormula.tsx
@@ -7,23 +7,45 @@ interface Props {
   children: Matrix<number | string>;
   block?: boolean;
   name?: string;
+  bracket?: Bracket;
 }
 
 function matrixToFormula(matrix: Matrix<number | string>): string {
   return matrix.map(row => row.join('&')).join('\\\\');
 }
 
-function wrapInBrackets(matrix: string): string {
-  return `\\left(\\begin{matrix}${matrix}\\end{matrix}\\right)`;
-}
+const symbols = {
+  '(': 'pmatrix',
+  '[': 'bmatrix',
+  '{': 'Bmatrix',
+  '|': 'vmatrix',
+  '||': 'Vmatrix',
+};
+
+export type Bracket = keyof typeof symbols;
+
+const wrapInBrackets = (bracket: Bracket) => (matrix: string): string => {
+  const symbol = symbols[bracket];
+  return `\\begin{${symbol}}${matrix}\\end{${symbol}}`;
+};
 
 const addName = (name: string | undefined) => (matrix: string): string => {
   if (typeof name === 'undefined') return matrix;
   return `${name} = ${matrix}`;
 };
 
-function MatrixFormula({ children: matrix, block, name }: Props): JSX.Element {
-  const formula = pipe(matrix, matrixToFormula, wrapInBrackets, addName(name));
+function MatrixFormula({
+  children: matrix,
+  block,
+  name,
+  bracket = '(',
+}: Props): JSX.Element {
+  const formula = pipe(
+    matrix,
+    matrixToFormula,
+    wrapInBrackets(bracket),
+    addName(name),
+  );
   return <Formula block={block}>{formula}</Formula>;
 }
 
