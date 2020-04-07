27fe9e0526c9150d9a9eceac2a2c621f50d66e1c
Romez <Romez1990@gmail.com>
2020-04-07 11:23:33 +0300

    feature(Formula): create formula component

---
+++ components/Math/Formula.tsx
@@ -0,0 +1,19 @@
+import React, { ReactNode } from 'react';
+import { KatexOptions, ParseError } from 'katex';
+// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+// @ts-ignore
+import TeX from '@matejmazur/react-katex';
+
+interface Props {
+  children: string;
+  block?: boolean;
+  errorColor?: string;
+  renderError?: (error: ParseError | TypeError) => ReactNode;
+  settings?: KatexOptions;
+}
+
+function Formula({ children, block }: Props): JSX.Element {
+  return <TeX block={block}>{children}</TeX>;
+}
+
+export default Formula;
