f28581f2a3e5196639d29595d967f4eba4b92f9f
Romez <Romez1990@gmail.com>
2020-04-22 09:55:47 +0300

    feature(App): add permission check

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,38 +1,76 @@
 import React, { useEffect, useState } from 'react';
 import { AppContext } from 'next/app';
-import { NextComponentType } from 'next/dist/next-server/lib/utils';
+import {
+  NextComponentType,
+  NextPageContext,
+} from 'next/dist/next-server/lib/utils';
 import { flow } from 'fp-ts/lib/function';
+import { fromNullable, isNone } from 'fp-ts/lib/Option';
 import StoreProvider from '../components/App/StoreProvider';
 import ThemeProvider from '../components/App/ThemeProvider';
 import store, { Store } from '../store';
+import AuthenticationService, {
+  Permission,
+} from '../src/AuthenticationService';
+import { RedirectionError, redirectToLogin } from '../src/Redirect';
+import ErrorPage from './_error';
 
 interface InitialProps {
   pageProps: object;
   store: Store;
+  hasPermission: boolean;
 }
 
 interface Props extends InitialProps {
   Component: NextComponentType;
 }
 
+export declare type ComponentType<P> = NextComponentType<
+  NextPageContext,
+  P,
+  P
+> & {
+  permission?: Permission;
+};
+
 App.getInitialProps = async ({
   Component,
   ctx,
 }: AppContext): Promise<InitialProps> => {
-  const { req } = ctx;
+  const { req, res } = ctx;
 
   if (!process.browser) {
     await store.profileStore.authenticate(req)();
   }
 
+  const TypedComponent = Component as ComponentType<object>;
+
+  const { permission } = TypedComponent;
+  const { profile } = store.profileStore;
+  const hasPermission = AuthenticationService.hasPermission(
+    profile,
+    fromNullable(permission),
+  );
+
+  const isProfileNone = isNone(profile);
+
+  if (!hasPermission && isProfileNone) {
+    if (process.browser || typeof req === 'undefined')
+      throw new RedirectionError(
+        'Redirecting from the browser is unacceptable',
+      );
+    await redirectToLogin(res, req.url);
+  }
+
   const pageProps =
-    typeof Component.getInitialProps !== 'undefined'
+    hasPermission && typeof Component.getInitialProps !== 'undefined'
       ? await Component.getInitialProps(ctx)
       : {};
 
   return {
     pageProps,
     store,
+    hasPermission,
   };
 };
 
@@ -40,6 +78,7 @@ function App({
   Component: Page,
   pageProps,
   store: preloadedState,
+  hasPermission,
 }: Props): JSX.Element {
   useEffect(flow(load, removeStyles), []);
 
@@ -57,6 +96,16 @@ function App({
     const jssStyles = document.querySelector('#jss-server-side');
     // eslint-disable-next-line no-unused-expressions
     jssStyles?.remove();
+  }
+
+  if (!hasPermission) {
+    return (
+      <StoreProvider>
+        <ThemeProvider>
+          <ErrorPage statusCode={404} />
+        </ThemeProvider>
+      </StoreProvider>
+    );
   }
 
   return (
