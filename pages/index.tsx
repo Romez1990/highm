7f0ad56c627b110dc6477dbff032f8991131a7a1
Romez <Romez1990@gmail.com>
2020-05-04 17:15:27 +0300

    feature(MainPage): modernize main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Container } from '@material-ui/core';
 import MainLayout from '../layouts/MainLayout';
 
 function MainPage(): JSX.Element {
-  return <MainLayout title="Main page">Main page</MainLayout>;
+  return (
+    <MainLayout title="Main page">
+      <Container>Main page</Container>
+    </MainLayout>
+  );
 }
 
 export default MainPage;
