314b346907c2aa774ed2b6adbf5868da91195521
Romez <Romez1990@gmail.com>
2020-05-12 16:08:43 +0300

    feature(RegistrationStore): create registration store

---
+++ store/Registration/useStore.ts
@@ -0,0 +1,16 @@
+import { createContext, useContext } from 'react';
+import { RegistrationStore } from './RegistrationStore';
+import StoreError from '../Root/StoreError';
+
+const RegistrationStoreContext = createContext<RegistrationStore | undefined>(
+  undefined,
+);
+
+function useRegistrationStore(): RegistrationStore {
+  const store = useContext(RegistrationStoreContext);
+  if (typeof store === 'undefined')
+    throw new StoreError('Registration store is not provided');
+  return store;
+}
+
+export { RegistrationStoreContext, useRegistrationStore };
