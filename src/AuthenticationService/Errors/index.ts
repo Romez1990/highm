9522ddf94d6e4a53ccdb66f1f90739549aed318a
Romez <Romez1990@gmail.com>
2020-05-12 16:26:17 +0300

    feature(Auth): create email verification functions

--- src/AuthenticationService/Errors/index.ts
+++ src/AuthenticationService/Errors/index.ts
@@ -4,3 +4,4 @@ export { default as LoginError } from './LoginError';
 export * from './LoginErrors';
 export { default as RegistrationError } from './RegistrationError';
 export * from './RegistrationErrors';
+export { default as EmailVerificationError } from './EmailVerificationError';
