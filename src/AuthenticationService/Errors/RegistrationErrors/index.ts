746627cc9ca6d73d4977a4e1f99db57a25a205b0
Romez <Romez1990@gmail.com>
2020-05-12 16:08:21 +0300

    feature(Auth): create registration functions

---
+++ src/AuthenticationService/Errors/RegistrationErrors/index.ts
@@ -0,0 +1,4 @@
+export { default as WrongRegistrationCodeError } from './WrongRegistrationCodeError';
+export { default as NamesMismatchError } from './NamesMismatchError';
+export { default as EmailTakenError } from './EmailTakenError';
+export { default as TooCommonPasswordError } from './TooCommonPasswordError';
--- src/AuthenticationService/Errors/index.ts
+++ src/AuthenticationService/Errors/index.ts
@@ -2,3 +2,5 @@ export { default as PermissionError } from './PermissionError';
 export { default as AuthenticationServiceError } from './AuthenticationServiceError';
 export { default as LoginError } from './LoginError';
 export * from './LoginErrors';
+export { default as RegistrationError } from './RegistrationError';
+export * from './RegistrationErrors';
