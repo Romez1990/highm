06947d130740015e91fe636814aa504959951b98
Romez <Romez1990@gmail.com>
2020-05-16 10:32:59 +0300

    feature(HttpsRedirect): create HttpsRedirect component

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /* eslint-disable */
-const { env } = require('./src/EnvConfig');
+const { env, envBool } = require('./src/EnvConfig');
 
 module.exports = {
   env: {
     SERVER_URL: env('SERVER_URL'),
+    USE_HTTPS: envBool('USE_HTTPS'),
   },
 };
