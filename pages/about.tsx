5b024091013f458b2e1c9f7df100b4ffbc5753f4
Romez <Romez1990@gmail.com>
2020-05-16 11:56:48 +0300

    feature(src): move AboutPage to src

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,13 +1,1 @@
-import React from 'react';
-import { Container } from '@material-ui/core';
-import MainLayout from '../layouts/MainLayout';
-
-function AboutPage(): JSX.Element {
-  return (
-    <MainLayout title="About page">
-      <Container>About page</Container>
-    </MainLayout>
-  );
-}
-
-export default AboutPage;
+export { default } from '../src/AboutPage';
