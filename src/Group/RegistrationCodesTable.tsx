ead87e90c1656bbab3c434246e360c199a30f3eb
Romez <Romez1990@gmail.com>
2020-05-19 17:25:03 +0300

    feature(Group): add the ability to print registration codes

--- src/Group/RegistrationCodesTable.tsx
+++ src/Group/RegistrationCodesTable.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { Fragment, RefObject } from 'react';
 import {
+  makeStyles,
+  createStyles,
   Table,
   TableBody,
   TableCell,
@@ -7,38 +9,59 @@ import {
   TableHead,
   TableRow,
 } from '@material-ui/core';
+import PrintRegistrationCodes from './PrintRegistrationCodes';
 import { UnregisteredUser } from '../User';
 
 interface Props {
+  printComponent: RefObject<PrintRegistrationCodes>;
+
   users(): UnregisteredUser[];
 }
 
-function RegistrationCodesTable({ users: getUsers }: Props): JSX.Element {
+const useStyles = makeStyles(() =>
+  createStyles({
+    displayNone: {
+      display: 'none',
+    },
+  }),
+);
+
+function RegistrationCodesTable({
+  printComponent,
+  users: getUsers,
+}: Props): JSX.Element {
   const users = getUsers();
 
+  const classes = useStyles();
+
   return (
-    <TableContainer>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>№</TableCell>
-            <TableCell>Name</TableCell>
-            <TableCell>Registration code</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {users.map((user, index) => (
-            <TableRow key={user.registrationCode}>
-              <TableCell>{index + 1}.</TableCell>
-              <TableCell>
-                {user.lastName} {user.firstName}
-              </TableCell>
-              <TableCell>{user.registrationCode}</TableCell>
+    <Fragment>
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>№</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell>Registration code</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {users.map((user, index) => (
+              <TableRow key={user.registrationCode}>
+                <TableCell>{index + 1}.</TableCell>
+                <TableCell>
+                  {user.lastName} {user.firstName}
+                </TableCell>
+                <TableCell>{user.registrationCode}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <div className={classes.displayNone}>
+        <PrintRegistrationCodes ref={printComponent} users={users} />
+      </div>
+    </Fragment>
   );
 }
 
