c3066731ce7cf69a999d1ecf43cc71a7550c1cdb
Romez <Romez1990@gmail.com>
2020-05-19 17:15:45 +0300

    refactor(User): create user compare function

--- src/User/User.ts
+++ src/User/User.ts
@@ -37,3 +37,9 @@ export declare type UnregisteredUserNew = TypeOf<typeof TUnregisteredUserNew>;
 
 export const TUser = union([TRegisteredUser, TUnregisteredUser]);
 export declare type User = TypeOf<typeof TUser>;
+
+export function userCompare(a: User, b: User): number {
+  if (a.lastName < b.lastName) return 1;
+  if (a.lastName > b.lastName) return -1;
+  return a.firstName.localeCompare(b.firstName);
+}
