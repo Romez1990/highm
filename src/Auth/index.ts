b0bd0002aa57ab38167b70b4b6b7fcc2d96ad6ed
Romez <Romez1990@gmail.com>
2020-05-19 07:12:35 +0300

    refactor(Registration): rearrange files

--- src/Auth/index.ts
+++ src/Auth/index.ts
@@ -1,4 +1,4 @@
 export { default as LoginPage } from './Login/LoginPage';
 export { default as LogoutPage } from './Logout/LogoutPage';
-export { default as RegistrationPage } from './Registration/RegistrationPage';
+export { default as RegistrationPage } from './Registration/RegistrationPageWrapper';
 export { default as EmailVerificationPage } from './EmailVerification/EmailVerificationPage';
