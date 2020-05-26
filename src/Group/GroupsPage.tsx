43c05bf3768d14e7820c308ad49793c0a9349529
Romez <Romez1990@gmail.com>
2020-05-26 11:57:13 +0300

    feature(Group): move fetchGroups function to separate module

--- src/Group/GroupsPage.tsx
+++ src/Group/GroupsPage.tsx
@@ -1,15 +1,10 @@
-import { IncomingMessage } from 'http';
 import React from 'react';
 import { NextPageContext } from 'next';
-import { pipe } from 'fp-ts/lib/pipeable';
-import { Task, of } from 'fp-ts/lib/Task';
-import { fold } from 'fp-ts/lib/TaskEither';
-import { array } from 'io-ts';
 import { MainLayout } from '../Layout';
 import GroupsTable from './GroupsTable';
-import HttpService from '../HttpService';
 import { Permission } from '../AuthenticationService';
-import { TGroupBasic, GroupBasic } from './Group';
+import { GroupBasic } from './Group';
+import fetchGroups from './fetchGroups';
 
 interface Props {
   groups: GroupBasic[];
@@ -24,19 +19,6 @@ GroupsPage.getInitialProps = async ({
     groups: await fetchGroups(req)(),
   };
 };
-
-function fetchGroups(req: IncomingMessage | undefined): Task<GroupBasic[]> {
-  const returnType = array(TGroupBasic);
-  return pipe(
-    HttpService.get('/group/', returnType, req),
-    fold(
-      err => {
-        throw err;
-      },
-      groups => of(groups),
-    ),
-  );
-}
 
 function GroupsPage({ groups }: Props): JSX.Element {
   return (
