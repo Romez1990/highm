49bc80fd3739b5ad8dc5bc5ce0db4af9b48ebbf5
Romez <Romez1990@gmail.com>
2020-05-26 11:57:13 +0300

    feature(Group): move fetchGroups function to separate module

---
+++ src/Group/fetchGroups.ts
@@ -0,0 +1,22 @@
+import { IncomingMessage } from 'http';
+import { pipe } from 'fp-ts/lib/pipeable';
+import { Task, of } from 'fp-ts/lib/Task';
+import { fold } from 'fp-ts/lib/TaskEither';
+import { array } from 'io-ts';
+import HttpService from '../HttpService';
+import { TGroupBasic, GroupBasic } from './Group';
+
+function fetchGroups(req: IncomingMessage | undefined): Task<GroupBasic[]> {
+  const returnType = array(TGroupBasic);
+  return pipe(
+    HttpService.get('/group/', returnType, req),
+    fold(
+      err => {
+        throw err;
+      },
+      groups => of(groups),
+    ),
+  );
+}
+
+export default fetchGroups;
--- src/Group/index.ts
+++ src/Group/index.ts
@@ -1,3 +1,4 @@
 export { default as GroupsPage } from './GroupsPage';
 export { default as GroupPage } from './GroupPage';
 export * from './Group';
+export { default as fetchGroups } from './fetchGroups';
