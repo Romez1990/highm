dfdc5609486d76c1e3f9d7d8ff018137f1e73323
Romez <Romez1990@gmail.com>
2020-05-28 08:02:39 +0300

    refactor(GroupPage): move fetchGroup function to separate module

--- src/Group/GroupPage.tsx
+++ src/Group/GroupPage.tsx
@@ -1,18 +1,12 @@
-import { IncomingMessage } from 'http';
-import { ParsedUrlQuery } from 'querystring';
 import React from 'react';
 import { NextPageContext } from 'next';
-import { pipe } from 'fp-ts/lib/pipeable';
-import { Option, some, none, isNone } from 'fp-ts/lib/Option';
-import { TaskOption } from 'fp-ts-contrib/lib/TaskOption';
-import { of } from 'fp-ts/lib/Task';
-import { fold } from 'fp-ts/lib/TaskEither';
+import { Option, isNone } from 'fp-ts/lib/Option';
 import { ErrorPage } from '../ErrorPage';
 import { MainLayout } from '../Layout';
 import GroupTable from './GroupTable';
-import HttpService, { NotFoundError } from '../HttpService';
 import { Permission } from '../AuthenticationService';
-import { TGroup, Group } from './Group';
+import fetchGroup from './fetchGroup';
+import { Group } from './Group';
 
 interface Props {
   group: Option<Group>;
@@ -28,23 +22,6 @@ GroupPage.getInitialProps = async ({
     group: await fetchGroup(query, req)(),
   };
 };
-
-function fetchGroup(
-  { name }: ParsedUrlQuery,
-  req?: IncomingMessage,
-): TaskOption<Group> {
-  if (typeof name !== 'string') return of(none);
-  return pipe(
-    HttpService.get(`/group/${encodeURIComponent(name)}/`, TGroup, req),
-    fold(
-      err => {
-        if (!(err instanceof NotFoundError)) throw err;
-        return of(none);
-      },
-      group => of(some(group)),
-    ),
-  );
-}
 
 function GroupPage({ group: groupOption }: Props): JSX.Element {
   if (isNone(groupOption)) {
