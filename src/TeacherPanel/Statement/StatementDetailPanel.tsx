d3c84dbd59b6c2c365451fb84fc58b8bc60812f4
Romez <Romez1990@gmail.com>
2020-06-01 13:26:37 +0300

    feature(StatementDetailPanel): add styles

--- src/TeacherPanel/Statement/StatementDetailPanel.tsx
+++ src/TeacherPanel/Statement/StatementDetailPanel.tsx
@@ -1,18 +1,28 @@
-import React, { Fragment } from 'react';
-import { Typography } from '@material-ui/core';
+import React from 'react';
+import { makeStyles, createStyles, Theme, Typography } from '@material-ui/core';
 import { TableLessonResult } from './LessonResult';
 
 interface Props {
   lessonResult: TableLessonResult;
 }
 
+const useStyles = makeStyles(({ spacing }: Theme) =>
+  createStyles({
+    root: {
+      padding: `${spacing(1)}px ${spacing(3)}px`,
+    },
+  }),
+);
+
 function StatementDetailPanel({ lessonResult }: Props): JSX.Element {
+  const classes = useStyles();
+
   return (
-    <Fragment>
+    <div className={classes.root}>
       <Typography>Serial number: {lessonResult.n}</Typography>
       <Typography>Grade: {lessonResult.grade}</Typography>
       <Typography>{lessonResult.points} points</Typography>
-    </Fragment>
+    </div>
   );
 }
 
