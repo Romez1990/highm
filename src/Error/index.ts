4c89563ac7fbf2321edf56e86fa13135d39d2b38
Romez <Romez1990@gmail.com>
2020-06-02 15:21:11 +0300

    Revert "temp"
    This reverts commit c6aab4f5

--- src/Error/index.ts
+++ src/Error/index.ts
@@ -1,5 +1,4 @@
 export { default as AppError } from './AppError';
 export { default as RegexError } from './RegexError';
-export { default as ErrorIdentifier } from './ErrorIdentifier';
-export * from './ErrorIdentifier';
+export * from './indentifyErrors';
 export * from './foldErrors';
--- src/HttpService/Errors/HttpErrorIdentifier.ts
+++
@@ -1,7 +0,0 @@
-import { ErrorIdentifier } from '../../Error';
-
-abstract class HttpErrorIdentifier<
-  Output extends Error
-> extends ErrorIdentifier<Output> {}
-
-export default HttpErrorIdentifier;
