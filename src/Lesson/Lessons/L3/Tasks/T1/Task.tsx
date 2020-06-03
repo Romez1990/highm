3058c028036901699465cd780cd7f0e8bc547921
Romez <Romez1990@gmail.com>
2020-06-03 13:27:34 +0300

    refactor(Lesson3/Task1): get tolerance from task for note

--- src/Lesson/Lessons/L3/Tasks/T1/Task.tsx
+++ src/Lesson/Lessons/L3/Tasks/T1/Task.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { makeStyles, createStyles, Theme } from '@material-ui/core';
+import React, { Fragment } from 'react';
+import { makeStyles, createStyles, Theme, Typography } from '@material-ui/core';
 import Formula from '../../../../../Math/Formula';
 import { TaskProps } from '../../../../Task';
 import { Task1Type } from './TaskType';
@@ -15,6 +15,9 @@ const useStyles = makeStyles(({ spacing }: Theme) =>
     span: {
       margin: spacing(0, 1.5),
     },
+    note: {
+      marginTop: spacing(2),
+    },
   }),
 );
 
@@ -28,13 +31,18 @@ function Task1({ children }: TaskProps): JSX.Element {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <Formula>{task.equation}</Formula>
-      <span className={classes.span}>на отрезке</span>
-      <Formula>{segment}</Formula>
-      <span className={classes.span}>с погрешностью</span>
-      <Formula>{epsilon}</Formula>
-    </div>
+    <Fragment>
+      <div className={classes.task}>
+        <Formula>{task.equation}</Formula>
+        <span className={classes.span}>на отрезке</span>
+        <Formula>{segment}</Formula>
+        <span className={classes.span}>с погрешностью</span>
+        <Formula>{epsilon}</Formula>
+      </div>
+      <Typography className={classes.note} variant="body2">
+        ответ указать с точностью {task.tolerance} знаков после запятой
+      </Typography>
+    </Fragment>
   );
 }
 
