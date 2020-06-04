763e04227e91e5efc7df103ed501a9894c1453c4
Romez <Romez1990@gmail.com>
2020-06-04 07:10:05 +0300

    feature(project): add background image

--- src/Theme/theme.ts
+++ src/Theme/theme.ts
@@ -1,11 +1,26 @@
 import { createMuiTheme } from '@material-ui/core';
 import grey from '@material-ui/core/colors/grey';
+import BackgroundImage from '../../assets/img/light-grey-flare.jpg';
 
 const lightTheme = createMuiTheme({
   palette: {
     primary: {
       main: '#64C51E',
       contrastText: '#FFF',
+    },
+  },
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        body: {
+          background: `url(${BackgroundImage})`,
+        },
+      },
+    },
+    MuiDrawer: {
+      paper: {
+        background: `rgba(255, 255, 255, 0.4)`,
+      },
     },
   },
 });
