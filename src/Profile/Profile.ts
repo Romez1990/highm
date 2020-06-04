967f6e17b07c78708de00319c03918bebbbdbae0
Romez <Romez1990@gmail.com>
2020-06-04 17:24:41 +0300

    refactor(Profile): move profile type to a separate type

--- src/Profile/Profile.ts
+++ src/Profile/Profile.ts
@@ -1,9 +1,16 @@
 import { type, string, boolean, literal, union, TypeOf } from 'io-ts';
 
+export const TProfileType = union([
+  literal('admin'),
+  literal('teacher'),
+  literal('student'),
+]);
+export declare type ProfileType = TypeOf<typeof TProfileType>;
+
 export const TProfile = type({
   firstName: string,
   lastName: string,
-  type: union([literal('admin'), literal('teacher'), literal('student')]),
+  type: TProfileType,
   darkMode: boolean,
 });
 export declare type Profile = TypeOf<typeof TProfile>;
