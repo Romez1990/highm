e3d1ab90b3820f59f1ebb8c625df0ed9ce2ff66b
Romez <Romez1990@gmail.com>
2020-06-07 08:17:30 +0300

    fix(Lessons): fix error displaying when copy tasks

--- src/Lesson/Lessons/L4/Tasks/T2/AnswerType.ts
+++ src/Lesson/Lessons/L4/Tasks/T2/AnswerType.ts
@@ -8,6 +8,6 @@ export declare type Answer2Type = Answer1Type;
 
 const validationSchema2 = validationSchema1;
 
-const initialValues2: Answer2Type = initialValues1;
+const initialValues2: Answer2Type = { ...initialValues1 };
 
 export { validationSchema2, initialValues2 };
