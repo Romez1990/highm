31806770cf39a011b95ed426f4706e260a53f47c
Romez <Romez1990@gmail.com>
2020-06-13 19:37:25 +0300

    refactor(foldErrors): change object type to A

--- src/Error/foldErrors.ts
+++ src/Error/foldErrors.ts
@@ -2,8 +2,8 @@ import { pipe } from 'fp-ts/lib/pipeable';
 import { reduce } from 'fp-ts/lib/Array';
 import { merge } from 'ramda';
 
-function foldErrors<A extends object>(errs: A[]): object {
-  return pipe(errs, reduce<object, object>({}, merge));
+function foldErrors<A extends object>(errs: A[]): A {
+  return pipe(errs, reduce<A, A>({} as A, merge));
 }
 
 export { foldErrors };
