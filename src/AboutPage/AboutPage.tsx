4d998567a5e5b0fd76ac4a6f959566698a001b43
Romez <Romez1990@gmail.com>
2020-06-16 10:12:27 +0300

    feature(AboutPage): add project description

--- src/AboutPage/AboutPage.tsx
+++ src/AboutPage/AboutPage.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import { Container } from '@material-ui/core';
+import { Container, Card, CardContent, Typography } from '@material-ui/core';
 import { MainLayout } from '../Layout';
 
 function AboutPage(): JSX.Element {
   return (
     <MainLayout title="About page">
-      <Container>About page</Container>
+      <Container>
+        <Card>
+          <CardContent>
+            <Typography>
+              HighM – это Web-приложение предназначенное для образовательного
+              учреждения среднего профессионального образования, в котором
+              осуществляется проведение практических работ по дисциплине
+              математика со студентами второго курса различных специальностей.
+            </Typography>
+          </CardContent>
+        </Card>
+      </Container>
     </MainLayout>
   );
 }
